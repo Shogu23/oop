06b313887ebcbd9e3470f55cc7d6f1ccf4a2faf6
Shogu23 <charles.marthouret@gmail.com>
2020-09-01 14:22:31 +0200

    fight

--- newindex.php
+++ newindex.php
@@ -1,17 +1,78 @@
 <?php
 
-class Warrior
+class Arena
 {
-    /** @var int */
-    private $health;
+    /** @var Fighter[] */
+    private $fighters;
 
-    /** @var int */
-    private $hitPoint;
+    /** @var Fighter|null */
+    private $winner;
+
+    public function __construct()
+    {
+        $this->fighters = [];
+        $this->winner = null;
+    }
+
+    public function add(Fighter $fighter): void
+    {
+        $this->fighters[] = $fighter;
+    }
+
+    public function fightAll(): void
+    {
+        while (null === $this->winner) {
+            $aliveFighters = $this->getAliveFighters();
+            if (count($aliveFighters) === 0) {
+                return;
+            } elseif (count($aliveFighters) === 1) {
+                $this->winner = $aliveFighters[0];
+            } else {
+                shuffle($aliveFighters);
+                $fighterOne = $aliveFighters[0];
+                $fighterTwo = $aliveFighters[1];
+                $fighterOne->takeDamage($fighterTwo->attack());
+                $fighterTwo->takeDamage($fighterOne->attack());
+            }
+        }
+
+        //Here
+    }
+
+    public function getWinner(): ?Fighter
+    {
+        return $this->winner;
+    }
+
+    /**
+     * @return Fighter[]
+     */
+    private function getAliveFighters(): array
+    {
+        $aliveFighters = [];
+        foreach ($this->fighters as $fighter) {
+            if ($fighter->isAlive()) {
+                $aliveFighters[] = $fighter;
+            }
+        }
+
+        return $aliveFighters;
+    }
+}
+
+
+abstract class Fighter
+{
+
+    protected $health;
+
+    protected $hitPoint;
 
     private $hitCount;
 
-    public function __construct(int $health, int $hitPoint) 
+    public function __construct(string $name, int $health, int $hitPoint) 
     {
+        $this->name = $name;
         $this->health = $health;
         $this->hitPoint = $hitPoint;
         $this->hitCount = 0; 
@@ -48,37 +109,23 @@ class Warrior
         return $this->hitCount;
     }
     
+} // Fin class Fighter
+
+class Warrior extends Fighter{
 
 } // Fin de class Warrior
 
-// ------------------------------- *************************** ------------------------------------ //
 
-class Wizard
-{
+class Wizard extends Fighter{
 
     const MAGIC_POINT_PER_ATTACK = 2;
-    /** @var int */
-    private $health;
-
-    /** @var int */
-    private $hitPoint;
-
-    /** @var int */
+    
     private $magicPoint;
 
-    private $hitCount;
-
-    public function __construct(int $health, int $hitPoint, int $magicPoint = 10) 
+    public function __construct(string $name, int $health, int $hitPoint, int $magicPoint = 10) 
     {
-        $this->health = $health;
-        $this->hitPoint = $hitPoint;
+        parent::__construct($name, $health, $hitPoint);
         $this->magicPoint = $magicPoint;
-        $this->hitCount = 0; 
-    }
-
-    public function isAlive()
-    {
-        return $this->health > 0;
     }
 
     public function attackWithMagic()
@@ -93,21 +140,6 @@ class Wizard
             return $this->hitPoint;
         }
     }
-
-    public function takeDamage(int $damage)
-    {
-        if ($this->isAlive())
-        {
-            $this->health -= $damage;
-            $this->hitCount++;
-        }
-    }
-
-    public function getHitCount()
-    {
-        return $this->hitCount;
-    }
-
 } // Fin de class Wizard
 
 $guerrierBourru = new Warrior(100, random_int(0, 10));
@@ -151,4 +183,21 @@ else if ($mageTrappu->isAlive())
 else if ($magePoillu->isAlive() && $mageTrappu->isAlive())
 {
     echo "After a long long fight, both mage went on a nap, and never woke up...";
+}
+
+$arena = new Arena();
+for ($i = 1; $i <= 10; $i++) {
+    $warrior = new Warrior("Number $i", 100, rand(0, 50));
+    $wizard = new Wizard("Number $i", 100, rand(25, 50));
+
+    $arena->add($warrior);
+    $arena->add($wizard);
+}
+
+$arena->fightAll();
+$winner = $arena->getWinner();
+if (null === $winner) {
+    echo "<h1>There is no winner!</h1>";
+} else {
+    echo "<h1>" . $winner . " has won!</h1>";
 }
